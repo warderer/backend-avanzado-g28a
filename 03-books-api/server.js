05f6edd4af5c18f9a8acfbe917d94008e2cd5ef4
César Guerra <warderer@gmail.com>
2024-04-24 21:37:15 -0600

    ✨ Add show server logs with Morgan

--- 03-books-api/server.js
+++ 03-books-api/server.js
@@ -1,5 +1,6 @@
 import { connect } from './config/database.js'
 import express from 'express'
+import morgan from 'morgan'
 import bookRoutes from './routes/bookRoutes.js'
 
 const PORT = process.env.PORT || 3000
@@ -8,6 +9,10 @@ connect() // Nos conectamos a la base de datos
 
 const api = express()
 api.use(express.json())
+// api.use(morgan('tiny'))
+morgan.token('host', (req, res) => req.hostname)
+morgan.token('param', (req, res, param) => req.params[param])
+api.use(morgan(':host :method :url :status :param[id] :res[content-length] - :response-time ms - :date'))
 
 // Aqui van las rutas
 api.use('/api/v1/books', bookRoutes)
